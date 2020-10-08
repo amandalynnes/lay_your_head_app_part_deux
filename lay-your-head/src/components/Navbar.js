168616c6694f3aa7b36b67f26d70f9c61df3bb99
Amanda Simmons <carpe737diem@gmail.com>
2020-10-08 17:10:01 -0400

    updated form, headings, and added more images to the assets folder

--- lay-your-head/src/components/Navbar.js
+++ lay-your-head/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { NavLink } from "react-router-dom"
-import { Menu, Layout } from "antd"
+import { Menu, Layout, Image } from "antd"
 import {
     Switch,
     Route,
@@ -8,14 +8,19 @@ import {
 import { NotFound } from "../pages/NotFound"
 import Registration from "../pages/Registration"
 import "./Navbar.css"
+import Logo from '../logo.png';
+
+
 const {Header} =Layout
 const Home = () => <h1>home</h1>
 export const Navbar = () => {
     return (
         <>
+        <Image width={100} height={100} src={Logo}></Image>
             <Menu  mode="horizontal">
             <Menu.Item>
-                <h1>Lay Your Head</h1>
+                
+                {/* <h1>Lay Your Head</h1> */}
                 </Menu.Item>
                 <Menu.Item>
                     <NavLink to="/">Home</NavLink>
