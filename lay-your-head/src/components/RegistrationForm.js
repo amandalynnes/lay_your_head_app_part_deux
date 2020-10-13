505a56ba9d33ff447e76f5de2ebac95667060c05
Kyle Thomas <mrkylethomas76@gmail.com>
2020-10-13 15:01:33 -0400

    Committing changes before merge

--- lay-your-head/src/components/RegistrationForm.js
+++ lay-your-head/src/components/RegistrationForm.js
@@ -3,102 +3,103 @@ import "../../src/App.css";
 import "antd/dist/antd.css";
 import { Form, Input, Button } from "antd";
 import { MailOutlined, UserOutlined, LockOutlined } from "@ant-design/icons";
-import * as firebase from "firebase/app"
-import { useAuthState } from 'react-firebase-hooks/auth';
+import * as firebase from "firebase/app";
+import { useAuthState } from "react-firebase-hooks/auth";
 // TODO: Implement redirection to home page after registration
 // TODO: Take out username field as firebase takes email and password only.
 
 export default function RegistrationForm() {
-    // const [user] = useAuthState()
-    const [form] = Form.useForm();
-    const [, forceUpdate] = useState(); // To disable submit button at the beginning.
-    useEffect(() => {
-        forceUpdate({});
-    }, []);
-    const onFinish = (values) => {
-        firebase.auth().createUserWithEmailAndPassword(values.email, values.password).catch(function(error) {
-            // Handle Errors here.
-            let errorCode = error.code;
-            let errorMessage = error.message;
-            console.log({errorCode, errorMessage})
-          });
-    };
-    return (
-        <div>
-            <h2>Because <strong>you</strong> deserve a place to lay your head.</h2>
-            <h4>Register Here</h4>
-            <Form
-                form={form}
-                name="horizontal_login"
-                layout="inline"
-                onFinish={onFinish}
+  // const [user] = useAuthState()
+  const [form] = Form.useForm();
+  const [, forceUpdate] = useState(); // To disable submit button at the beginning.
+  useEffect(() => {
+    forceUpdate({});
+  }, []);
+  const onFinish = (values) => {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(values.email, values.password)
+      .catch(function (error) {
+        // Handle Errors here.
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        console.log({ errorCode, errorMessage });
+      });
+  };
+  return (
+    <div>
+      <h2>
+        Because <strong>you</strong> deserve a place to lay your head.
+      </h2>
+      <h4>Register Here</h4>
+      <Form
+        form={form}
+        name="horizontal_login"
+        layout="inline"
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Please input your email!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<MailOutlined className="site-form-item-icon" />}
+            placeholder="email"
+          />
+        </Form.Item>
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="username"
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item shouldUpdate={true}>
+          {() => (
+            <Button
+              type="primary"
+              htmlType="submit"
+              disabled={
+                !form.isFieldsTouched(true) ||
+                form.getFieldsError().filter(({ errors }) => errors.length)
+                  .length
+              }
             >
-                 <Form.Item
-                    name="email"
-                    rules={[
-                        {
-                            required: true,
-                            message: "Please input your email!",
-                        },
-                    ]}
-                >
-                    <Input
-                        prefix={<MailOutlined className="site-form-item-icon" />}
-                        placeholder="email"
-                    />
-                </Form.Item>
-                <Form.Item
-                    name="username"
-                    rules={[
-                        {
-                            required: true,
-                            message: "Please input your username!",
-                        },
-                    ]}
-                >
-                    <Input
-                        prefix={<UserOutlined className="site-form-item-icon" />}
-                        placeholder="username"
-                    />
-                </Form.Item>
-                <Form.Item
-                    name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: "Please input your password!",
-                        },
-                    ]}
-                >
-                    <Input
-                        prefix={<LockOutlined className="site-form-item-icon" />}
-                        type="password"
-                        placeholder="Password"
-                    />
-                </Form.Item>
-                <Form.Item shouldUpdate={true}>
-                    {() => (
-                        <Button
-                            type="primary"
-                            htmlType="submit"
-                            disabled={
-                                !form.isFieldsTouched(true) ||
-                                form.getFieldsError().filter(({ errors }) => errors.length)
-                                    .length
-                            }
-                        >
-                            Sign up
-                        </Button>
-                    )}
-                </Form.Item>
-            </Form>
-            {/* Link the following button to the registration page */}
-            <Button type="link" htmlType="button">
-                Go back to Log in page
+              Sign in {console.log("The button is being called")}
+            </Button>
+          )}
+        </Form.Item>
+      </Form>
+      <Button type="link" htmlType="button" href="/login">
+        Go back to Log in page
       </Button>
-      {/* {user && <h1>Welcome!</h1>} */}
-            {/* Link the following button to Google login */}
-            {/* <Button>Login with Google!</Button> */}
-        </div>
-    );
+    </div>
+  );
 }
--- lay-your-head/src/pages/login/LoginForm.js
+++ lay-your-head/src/pages/login/LoginForm.js
@@ -1,26 +1,29 @@
-import React from "react"
+import React from "react";
 import { Form, Input, Button } from "antd";
-import { auth } from "../../firebase"
-import * as firebase from "firebase/app"
+import { auth } from "../../firebase";
+import * as firebase from "firebase/app";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
 
 const LoginForm = () => {
-    const [form] = Form.useForm();
-    const googleSignUp = () => {
-      const provider = new firebase.auth.GoogleAuthProvider();
-      auth.signInWithPopup(provider);
-    }
-  
-    
-    const onFinish = (values) => {
-      firebase.auth().signInWithEmailAndPassword(values.username, values.password).catch(function(error) {
+  const [form] = Form.useForm();
+  const googleSignUp = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+
+  const onFinish = (values) => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(values.username, values.password)
+      .catch(function (error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-          console.log({errorCode, errorMessage})
-      })
-    };
-    return <Form
+        console.log({ errorCode, errorMessage });
+      });
+  };
+  return (
+    <Form
       form={form}
       className="form"
       name="horizontal_login"
@@ -62,22 +65,21 @@ const LoginForm = () => {
             htmlType="submit"
             disabled={
               !form.isFieldsTouched(true) ||
-              form.getFieldsError().filter(({ errors }) => errors.length)
-                .length
+              form.getFieldsError().filter(({ errors }) => errors.length).length
             }
           >
             Sign in
           </Button>
         )}
       </Form.Item>
-      <Button type="link" htmlType="button">
+      {/* Link the following button to the registration page */}
+      <Button type="link" htmlType="button" href="/register">
         New User? Sign Up Here
-    </Button>
+      </Button>
       <br />
       <Button onClick={googleSignUp}>Login with Google!</Button>
     </Form>
-    {/* Link the following button to the registration page */ }
-    {/* Link the following button to Google login */ }
-  }
+  );
+};
 
-  export default LoginForm
+export default LoginForm;
