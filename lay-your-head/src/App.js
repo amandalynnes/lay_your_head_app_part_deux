a0c22f7856474a1186917a7d3f68311df48363a0
Amanda Simmons <carpe737diem@gmail.com>
2020-10-14 14:11:54 -0400

    home page only renders on home page, shelter api logging to console, set up example api structure in api.js

--- lay-your-head/src/App.js
+++ lay-your-head/src/App.js
@@ -6,15 +6,9 @@ import HomePage from "./pages/HomePage";
  function App() {
 
   return (
-    // <>
-    //   {/* <div className="App">
-    //     <h1>Lay Your Head</h1>
-    //   </div> */}
-    //   <Registration />
-    // </>
+   
     <div className="App">
       <Navbar />
-      <HomePage />
     </div>
   );
 }
--- lay-your-head/src/utils/shelterApi.js
+++ lay-your-head/src/utils/shelterApi.js
@@ -3,7 +3,12 @@
 async function shelterApi() {
     try {
 // use fetch and .then
+fetch("https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Public_Service_WebMercator/MapServer/25/query?where=1%3D1&outFields=FACILITY_NAME,PROVIDER,ADDRESS,CITY,STATE,ZIP,LATITUDE,LONGITUDE,WARD,NUMBER_OF_BEDS,AGES_SERVED&outSR=4326&f=json")
+        .then(response => response.json())
+        .then(data => console.log(data));
     } catch (err) {
-
+        console.error(err);
     }
 }
+
+export default shelterApi
