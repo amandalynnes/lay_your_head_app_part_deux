2b22577ff35d7c8326d73c7f17b5aab8ce14d8dc
Santiago Toro <tiagoelcoder@gmail.com>
2020-10-15 02:42:48 -0700

    Working code, almost got it

--- lay-your-head/src/utils/shelterApi.js
+++ lay-your-head/src/utils/shelterApi.js
@@ -1,11 +1,11 @@
 //use fetch to get the info from the shelter api using the query url
-
+export const SHELTER_URL = "https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Public_Service_WebMercator/MapServer/25/query?where=1%3D1&outFields=FACILITY_NAME,PROVIDER,ADDRESS,CITY,STATE,ZIP,LATITUDE,LONGITUDE,WARD,NUMBER_OF_BEDS,AGES_SERVED&outSR=4326&f=json"
 async function shelterApi() {
     try {
 // use fetch and .then
 fetch("https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Public_Service_WebMercator/MapServer/25/query?where=1%3D1&outFields=FACILITY_NAME,PROVIDER,ADDRESS,CITY,STATE,ZIP,LATITUDE,LONGITUDE,WARD,NUMBER_OF_BEDS,AGES_SERVED&outSR=4326&f=json")
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(data =>data);
     } catch (err) {
         console.error(err);
     }
