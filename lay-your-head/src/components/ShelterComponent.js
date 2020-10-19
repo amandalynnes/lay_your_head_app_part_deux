fbaa289faa0c667c7287ebf7e0ec5368034fb888
Santiago Toro <tiagoelcoder@gmail.com>
2020-10-19 13:50:37 -0700

    modified error message

--- lay-your-head/src/components/ShelterComponent.js
+++ lay-your-head/src/components/ShelterComponent.js
@@ -1,11 +1,11 @@
 import React from "react"
-import { Descriptions, Statistic, Card } from 'antd';
+import { Descriptions, Statistic, Card} from 'antd';
 import  { SHELTER_URL } from "../utils/shelterApi"
 import {useQuery} from "react-query"
 import { GoogleMap,  LoadScript, Marker } from '@react-google-maps/api';
 
 export const ShelterComponent = () => {
-
+   
     async function fetchData() {
         const response = await fetch(SHELTER_URL)
         const data = await response.json()
@@ -14,13 +14,13 @@ export const ShelterComponent = () => {
     const {status, data, error} = useQuery("shelter", fetchData)
     // const {features} = data
     if(status==="loading"){return <p>Loading </p>}
-    if(error) return <p>some worng</p>
+    if(error) return <p>something went worng...</p>
     if(data)
     return (
         <>         
      {data.features.map((shelter, i)=> {  
-           return <ShelterCard key ={i} shelter={shelter}/>
-       })}
+         return <ShelterCard key ={i} shelter={shelter}/>
+        })}
     </>
     )
 }
@@ -44,7 +44,7 @@ const ShelterCard = ({shelter}) => {
             {/* Consider making the contact INFO as a button instead to send a message or something, since there's no phone number */}
             <Descriptions.Item label="Info">Contact</Descriptions.Item>
             <Descriptions.Item label="Address"> {attributes.ADDRESS}</Descriptions.Item>
-            <Descriptions.Item label="State and Zipcode"> {attributes.STATE, attributes.ZIP}</Descriptions.Item>
+<Descriptions.Item label="State and Zipcode"> {attributes.ZIP}, {attributes.STATE}</Descriptions.Item>
         </Descriptions>
         <Statistic title="Number of Beds" value={attributes.NUMBER_OF_BEDS} />
        
